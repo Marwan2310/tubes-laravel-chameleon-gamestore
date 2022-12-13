086e6584456e3f85b99b103a889dd7c9a3aa2e54
yazman-giv <myazman13@gmail.com>
2022-12-13 10:46:32 +0700

    mengedit halaman ergistrasi

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -11,7 +11,6 @@
                 <div class="">
                   <img src="img/register3.jpg" class="img-fluid rounded-start" width="1300">
                 </div>
-                
                 <div class="row">
                   <div class="login justify-content-center col-md-6 mb-4">
                     {{-- <div class="form-outline"> --}}
@@ -22,16 +21,13 @@
                         <input type="" placeholder="Username">
                       </form>
                     </div>
-                    {{-- </div> --}}
                     <div class="login justify-content-center col-md-6 mb-4">
-                      {{-- <div class="form-outline"> --}}
                       <form action="" width="400px">  
                         <label for="">Email</label>
                         <input type="email" placeholder="example@gmail.com">
                         <label for="">Password</label>
                         <input type="password" placeholder="Password">
                       </form>
-                      {{-- </div> --}}
                     </div>
                   </div>
                 <button>Register</button>
@@ -44,19 +40,3 @@
         </div>
       </div>
 @endsection
-
-<!-- 2 column grid layout with text inputs for the first and last names -->
-{{-- <div class="row">
-  <div class="col-md-6 mb-4">
-    <div class="form-outline">
-      <input type="text" id="form3Example1" class="form-control" />
-      <label class="form-label" for="form3Example1">First name</label>
-    </div>
-  </div>
-  <div class="col-md-6 mb-4">
-    <div class="form-outline">
-      <input type="text" id="form3Example2" class="form-control" />
-      <label class="form-label" for="form3Example2">Last name</label>
-    </div>
-  </div>
-</div> --}}
