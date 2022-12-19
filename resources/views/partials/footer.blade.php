96d148bd439ea9cd045d003267def61a879b1f8b
yazman-giv <myazman13@gmail.com>
2022-12-19 18:54:32 +0700

    membuat-footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,9 +1,11 @@
-<footer>
-  <div class="container mt-5">
-    <div class="row">
-      <div class="col-lg-12">
-        <h4 style="color: aliceblue; text-align:center">FOOTER</h4>
-      </div>
-    </div>
-  </div>
-</footer>
+<!-- footer -->
+  <footer class="light-blue darken-4 text-center pb-3 mt-5" style="color:white"><br>
+    <h3>Chameleon</h3><br>
+    <p>Find us on various social media platforms</p>
+    <a href=""><i class="bi bi-facebook" style="font-size: 2rem; color :white"></i></a>
+    <a href="https://github.com/Marwan2310/tubes-laravel-chameleon-gamestore"><i class="bi bi-github ms-3" style="font-size: 2rem; color :white"></i></a>
+    <a href=""><i class="bi bi-instagram ms-3" style="font-size: 2rem; color :white"></i></a>
+    <a href=""><i class="bi bi-twitter ms-3" style="font-size: 2rem; color :white"></i></a>
+    <a href=""><i class="bi bi-youtube ms-3" style="font-size: 2rem; color :white"></i></a>
+    <p>Copyright © 2022 Chameleon</p>
+  </footer>
