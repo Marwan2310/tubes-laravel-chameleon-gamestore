259304865bee0c7f937c9bc10c3f028920d9de55
yazman-giv <myazman13@gmail.com>
2022-12-19 19:45:31 +0700

    edit-halaman-login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -15,10 +15,68 @@
 </div>
 @endif
 
+<!-- Section: Design Block -->
+<section class="background-radial-gradient overflow-hidden">
+<h1 class="h3 text-center mt-3">Please Login</h1>
+<hr style="color: aqua;">
+  <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
+    <div class="row gx-lg-5 align-items-center mb-5">
+      <div class="col-lg-6 mb-5 mb-lg-0" style="z-index: 10">
+        <h1 class="my-5 display-5 fw-bold ls-tight" style="color: hsl(218, 81%, 95%)">
+          Welcome<br />
+          <span style="color: hsl(218, 81%, 75%)">back</span>
+        </h1>
+        <p class="mb-4 opacity-70" style="color: hsl(218, 81%, 85%)">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Temporibus, expedita iusto veniam atque, magni tempora mollitia
+          dolorum consequatur nulla, neque debitis eos reprehenderit quasi
+          ab ipsum nisi dolorem modi. Quos?
+        </p>
+      </div>
 
-      <h1 class="h3 text-center">Please Login</h1>
-      <hr style="color: aqua;">
-<div class="container">
+      <div class="col-lg-6 mb-5 mb-lg-0 position-relative">
+        <div id="radius-shape-1" class="position-absolute rounded-circle shadow-5-strong"></div>
+        <div id="radius-shape-2" class="position-absolute shadow-5-strong"></div>
+
+        <div class="card bg-glass">
+          <div class="card-body px-4 py-5 px-md-5">
+            <form action="/login" method="post">
+              @csrf
+            <h3 class="text-center" style="color: #00005C">Chameleon</h3>
+            <hr style="color: #00005C;">
+            <form>
+              <!-- Email input -->
+              <div class="form-outline mb-4">
+                <label for="email">Email</label>  
+              <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" autofocus required>
+                
+              </div>
+              @error('email')
+          <div class="invalid-feedback">
+            {{ $message }}
+          </div>
+          @enderror
+              <!-- Password input -->
+              <div class="form-outline mb-4">
+                <label for="password">Password</label>
+                <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
+              </div>
+              <!-- Submit button -->
+              <button type="submit" class="btn btn-primary btn-block mb-4">
+                Login
+              </button>
+              <p>
+                Don't have an account? </p><p> <a href="/register" style="text-decoration: none">Sign up</a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+      
+{{-- <div class="container">
   <div class=" row">
     <div class="col">
       <div class="card mb-4 justify-content-center" style="background-image:url(img/bg2.png);">
@@ -54,6 +112,6 @@
       </div>
     </div>
   </div>
-</div>
+</div> --}}
 @endsection
 
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg bg-transparent">
   <div class="container">
     <a class="navbar-brand" href="#">
-      <img src="img/logo2.png" alt="Bootstrap" width="60px" height="45px">Chameleon
+      <img src="img/logo2.png" alt="Bootstrap" width="60px" height="45px" style="color: aliceblue">Chameleon
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
