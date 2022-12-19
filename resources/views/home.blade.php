2a2032cffab89d0c93961396765f0cc9920bc176
yazman-giv <myazman13@gmail.com>
2022-12-19 09:49:52 +0700

    membuat-halaman-home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,20 +9,61 @@
     <br><br>
     <h6>Welcome To Chameleon</h6>
     <h4><em>Browse</em> Our Popular Games Here</h4>
-
   </div>
-
   <br>
   <br>
-
-  <h4>Rekomendasi</h4>
   <br>
-
-
-
-
-
-
-
+  <div class="rekomendasi-game">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="heading-section">
+        <h4>Rekomendasi Game</h4><br>
+        </div>
+        <div class="row">
+          <div class="col-lg-3 col-sm-6">
+            <div class="item">
+              <img src="img/login.jpg" alt="" width="215" height="215"><br>
+                <h4><a href="" style="text-decoration: none; color:aliceblue">Nama Game</a><br><span><a href="" style="text-decoration: none; color:aliceblue">Penerbit</a></span><br><i class="bi bi-download" style="color: beige"> 100 Mb</i></h4>
+            </div>
+          </div>
+          <div class="col-lg-3 col-sm-6">
+            <div class="item">
+              <img src="img/login.jpg" alt="" width="215" height="215"><br>
+                <h4><a href="" style="text-decoration: none; color:aliceblue">Nama Game</a><br><span><a href="" style="text-decoration: none; color:aliceblue">Penerbit</a></span><br><i class="bi bi-download" style="color: beige"> 100 Mb</i></h4>
+            </div>
+          </div>
+          <div class="col-lg-3 col-sm-6">
+            <div class="item">
+              <img src="img/login.jpg" alt="" width="215" height="215"><br>
+                <h4><a href="" style="text-decoration: none; color:aliceblue">Nama Game</a><br><span><a href="" style="text-decoration: none; color:aliceblue">Penerbit</a></span><br><i class="bi bi-download" style="color: beige"> 100 Mb</i></h4>
+            </div>
+          </div>
+          <div class="col-lg-3 col-sm-6">
+            <div class="item">
+              <img src="img/login.jpg" alt="" width="215" height="215"><br>
+                <h4><a href="" style="text-decoration: none; color:aliceblue">Nama Game</a><br><span><a href="" style="text-decoration: none; color:aliceblue">Penerbit</a></span><br><i class="bi bi-download" style="color: beige"> 100 Mb</i></h4>
+            </div>
+          </div>
+          <div class="col-lg-3 col-sm-6">
+            <div class="item">
+              <img src="img/login.jpg" alt="" width="215" height="215"><br>
+                <h4><a href="" style="text-decoration: none; color:aliceblue">Nama Game</a><br><span><a href="" style="text-decoration: none; color:aliceblue">Penerbit</a></span><br><i class="bi bi-download" style="color: beige"> 100 Mb</i></h4>
+            </div>
+          </div>
+          <div class="col-lg-3 col-sm-6">
+            <div class="item">
+              <img src="img/login.jpg" alt="" width="215" height="215"><br>
+                <h4><a href="" style="text-decoration: none; color:aliceblue">Nama Game</a><br><span><a href="" style="text-decoration: none; color:aliceblue">Penerbit</a></span><br><i class="bi bi-download" style="color: beige"> 100 Mb</i></h4>
+            </div>
+          </div>
+          <div class="col-lg-12">
+            <div class="main-button">
+              <a href="/game" style="text-decoration: none">Selengkapnya</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
   @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,6 +11,8 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css"> <!-- My CSS -->
   <link rel="stylesheet" href="/css/style.css">
   <link rel="stylesheet" href="/css/fontawesome.css">
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
 
 
@@ -30,6 +32,8 @@
 
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+
+  @include('partials.footer')
 
 </body>
 
