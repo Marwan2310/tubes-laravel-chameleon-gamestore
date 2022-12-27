d1acd37f773b87bd9bea19d832a7e7f917deb459
Marwan2310 <imaroneu@gmail.com>
2022-12-27 11:31:06 +0700

    menambah fitur play game

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts/main')
+@extends('layouts.main')
 
 
 @section('container')
@@ -11,8 +11,8 @@
     <h4><em>Browse</em> Our Popular Games Here</h4>
   </div> --}}
 
-  
-{{-- slider --}}
+
+  {{-- slider --}}
   <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
     <div class="carousel-indicators">
       <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -25,7 +25,7 @@
         <div class="carousel-caption d-none d-md-block" style="background-color: rgba(0, 0, 0, 0.6); border-radius: 23px;">
           <h5>Game</h5>
           <p>Find the most popular and newest games in the Chameleon game store and let's play</p>
-          <a href="/game" class="btn btn-info btn-rounded">View more</a>
+          <a href="/posts" class="btn btn-info btn-rounded">View more</a>
         </div>
       </div>
       <div class="carousel-item">
@@ -61,48 +61,50 @@
     <div class="row">
       <div class="col-lg-12">
         <div class="heading-section">
-        <h4>Game recommendations</h4><br>
+          <h4>Free to Play Game</h4><br>
         </div>
         <div class="row">
+
           <div class="col-lg-3 col-sm-6">
             <div class="item">
-              <img src="img/valo.jpg" alt="" width="215" height="215"><br> 
-                <h4><a href="" style="text-decoration: none; color:aliceblue">Nama Game</a><br><span><a href="" style="text-decoration: none; color:aliceblue">Penerbit</a></span><br><i class="bi bi-download" style="color: beige"> 100 Mb</i></h4>
+              <img src="img/tikustanah.png" alt="" width="215" height="215"><br>
+              <h4 style="text-decoration: none; color:aliceblue">Pukul Tikus Tanah</h4><br><br>
+              <a href="/pukultikus" role="button" class="btn btn-outline-info btn-rounded text-white" data-mdb-ripple-color="dark">Let's Play
+              </a>
             </div>
           </div>
+
           <div class="col-lg-3 col-sm-6">
             <div class="item">
-              <img src="img/login.jpg" alt="" width="215" height="215"><br>
-                <h4><a href="" style="text-decoration: none; color:aliceblue">Nama Game</a><br><span><a href="" style="text-decoration: none; color:aliceblue">Penerbit</a></span><br><i class="bi bi-download" style="color: beige"> 100 Mb</i></h4>
+              <img src="img/2048_logo.png" alt="" width="215" height="215"><br>
+              <h4 style="text-decoration: none; color:aliceblue">2048</h4><br><br>
+              <a href="/2048" role="button" class="btn btn-outline-info btn-rounded text-white" data-mdb-ripple-color="dark">Let's Play
+              </a>
             </div>
           </div>
+
           <div class="col-lg-3 col-sm-6">
             <div class="item">
-              <img src="img/login.jpg" alt="" width="215" height="215"><br>
-                <h4><a href="" style="text-decoration: none; color:aliceblue">Nama Game</a><br><span><a href="" style="text-decoration: none; color:aliceblue">Penerbit</a></span><br><i class="bi bi-download" style="color: beige"> 100 Mb</i></h4>
+              <img src="img/valo.jpg" alt="" width="215" height="215"><br>
+              <h4 style="text-decoration: none; color:aliceblue">Pukul Tikus Tanah</h4><br><br>
+              <a href="/pukultikus" role="button" class="btn btn-outline-info btn-rounded text-white" data-mdb-ripple-color="dark">Let's Play
+              </a>
             </div>
           </div>
+
           <div class="col-lg-3 col-sm-6">
             <div class="item">
-              <img src="img/login.jpg" alt="" width="215" height="215"><br>
-                <h4><a href="" style="text-decoration: none; color:aliceblue">Nama Game</a><br><span><a href="" style="text-decoration: none; color:aliceblue">Penerbit</a></span><br><i class="bi bi-download" style="color: beige"> 100 Mb</i></h4>
+              <img src="img/valo.jpg" alt="" width="215" height="215"><br>
+              <h4 style="text-decoration: none; color:aliceblue">Pukul Tikus Tanah</h4><br><br>
+              <a href="/pukultikus" role="button" class="btn btn-outline-info btn-rounded text-white" data-mdb-ripple-color="dark">Let's Play
+              </a>
             </div>
           </div>
-          <div class="col-lg-3 col-sm-6">
-            <div class="item">
-              <img src="img/login.jpg" alt="" width="215" height="215"><br>
-                <h4><a href="" style="text-decoration: none; color:aliceblue">Nama Game</a><br><span><a href="" style="text-decoration: none; color:aliceblue">Penerbit</a></span><br><i class="bi bi-download" style="color: beige"> 100 Mb</i></h4>
-            </div>
-          </div>
-          <div class="col-lg-3 col-sm-6">
-            <div class="item">
-              <img src="img/login.jpg" alt="" width="215" height="215"><br>
-                <h4><a href="" style="text-decoration: none; color:aliceblue">Nama Game</a><br><span><a href="" style="text-decoration: none; color:aliceblue">Penerbit</a></span><br><i class="bi bi-download" style="color: beige"> 100 Mb</i></h4>
-            </div>
-          </div>
+
+
           <div class="col-lg-12">
             <div class="main-button">
-              <a href="/game" style="text-decoration: none">View more</a>
+              <a href="/categories" style="text-decoration: none">View more</a>
             </div>
           </div>
         </div>
@@ -110,36 +112,41 @@
     </div>
   </div>
 
-  {{-- rekomendasi category --}}
+
+
+
+  <!-- Wishlist -->
+  @auth
+
   <div class="body">
     <div class="row">
       <div class="col-lg-12">
         <div class="heading-section">
-        <h4>Category recommendations</h4><br>
+          <h4>My Library</h4><br>
         </div>
         <div class="row">
           <div class="col-lg-3 col-sm-6">
             <div class="item">
               <img src="img/login.jpg" alt="" width="215" height="215"><br>
-                <h4><a href="" style="text-decoration: none; color:aliceblue">Nama Category</a><br><br><i class="" style="color: beige">game</i></h4>
+              <h4><a href="" style="text-decoration: none; color:aliceblue">Nama Category</a><br><br><i class="" style="color: beige">game</i></h4>
             </div>
           </div>
           <div class="col-lg-3 col-sm-6">
             <div class="item">
               <img src="img/login.jpg" alt="" width="215" height="215"><br>
-                <h4><a href="" style="text-decoration: none; color:aliceblue">Nama Category</a><br><br><i class="" style="color: beige">game</i></h4>
+              <h4><a href="" style="text-decoration: none; color:aliceblue">Nama Category</a><br><br><i class="" style="color: beige">game</i></h4>
             </div>
           </div>
           <div class="col-lg-3 col-sm-6">
             <div class="item">
               <img src="img/login.jpg" alt="" width="215" height="215"><br>
-                <h4><a href="" style="text-decoration: none; color:aliceblue">Nama Category</a><br><br><i class="" style="color: beige">game</i></h4>
+              <h4><a href="" style="text-decoration: none; color:aliceblue">Nama Category</a><br><br><i class="" style="color: beige">game</i></h4>
             </div>
           </div>
           <div class="col-lg-3 col-sm-6">
             <div class="item">
               <img src="img/login.jpg" alt="" width="215" height="215"><br>
-                <h4><a href="" style="text-decoration: none; color:aliceblue">Nama Category</a><br><br><i class="" style="color: beige">game</i></h4>
+              <h4><a href="" style="text-decoration: none; color:aliceblue">Nama Category</a><br><br><i class="" style="color: beige">game</i></h4>
             </div>
           </div>
           <div class="col-lg-12">
@@ -151,5 +158,6 @@
       </div>
     </div>
   </div>
+  @endauth
 
   @endsection
